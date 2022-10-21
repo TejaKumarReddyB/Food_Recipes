c3934251362d733d4afacffcee9444b70d1c8c0d
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-10-21 14:00:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,8 @@ function App() {
     e.preventDefault();
     console.log("Searching");
     try{
-      const url=`https://api.themoviedb.org/3/search/movie?api_key=bcc4ff10c2939665232d75d8bf0ec093&query=${query}`;
+      //const url=`https://api.themoviedb.org/3/search/movie?api_key=bcc4ff10c2939665232d75d8bf0ec093&query=${query}`;
+      const url=`https://api.themoviedb.org/3/search/movie?api_key=43fdd57546f8dbd08439a5aa0c4434c9&query=${query}`;
       const res= await fetch(url);
       const data= await res.json();
       console.log(data);
